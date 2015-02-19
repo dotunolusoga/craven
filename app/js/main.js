010da1d0d23a181a10dfa294956ca438f81e07b5
Dotun Olusoga <adedotun.olusoga@gmail.com>
2015-02-19 15:50:53 -0600

    changes to build/watch scripts, created jade boilerplate for sections of the app

--- app/js/main.js
+++ app/js/main.js
@@ -10,3 +10,55 @@ function hello() {
 var $tbody = $('#tbody'),
     FIREBASE_URL = 'https://craven.firebaseio.com',
     fb           = new Firebase(FIREBASE_URL);
+
+
+
+////////////////////////////////
+// Login/Logout Functionality //
+////////////////////////////////
+
+
+  $('.register').click(function(event){
+    event.preventDefault();
+    var $form = $($(this).closest('form')),
+        email = $form.find('[type="text"]').val(),
+        pass = $form.find('[type="password"]').val();
+    fb.createUser({
+        email: email,
+        password: pass
+      },
+      function(err){
+        if(!err){
+              fb.authWithPassword({
+                email: email,
+                password: pass
+              },
+                function(err, auth){
+                    location.reload(true);
+              }
+            );
+        } else {}
+      }
+    );
+
+  });
+
+  $('.loginButton').click(function(event){
+    event.preventDefault();
+
+    var $form = $($(this).closest('form')),
+        email = $form.find('[type="text"]').val(),
+        pass = $form.find('[type="password"]').val();
+
+    fb.authWithPassword({
+      email    : email,
+      password : pass
+      }, function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        location.reload(true);
+        console.log("Authenticated successfully with payload:", authData);
+      }
+    });
+  });
--- build.sh
+++ build.sh
@@ -17,6 +17,8 @@ mkdir public/vendor
 cp bower_components/jquery/dist/jquery.min.js public/vendor
 cp bower_components/lodash/lodash.min.js public/vendor
 cp bower_components/firebase/firebase.js public/vendor
+cp bower_components/foundation/css/foundation.css public/vendor
+
 
 ###############################
 # Jade (npm intalled locally) #
