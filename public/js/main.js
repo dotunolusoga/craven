e2d87bea5ca684f09a7119c08ecbdf23dd3effe5
Dotun Olusoga <adedotun.olusoga@gmail.com>
2015-02-22 09:19:17 -0600

    started work on UX design/flow, worked on the 'Add Book Page' functionality, surmounted a major mental hurdle

--- public/js/main.js
+++ public/js/main.js
@@ -56,9 +56,48 @@ var $tbody = $('#tbody'),
       }, function(error, authData) {
       if (error) {
         console.log("Login Failed!", error);
+        alert("Login Failed! Please check your username & password");
       } else {
         location.reload(true);
         console.log("Authenticated successfully with payload:", authData);
       }
     });
+  });
+
+  //if authenticated, go to app page
+
+  fb.child('users').once('value', function(snap){
+    if (fb.getAuth()) {
+      $('.login').toggleClass('hidden');
+      $('.statusContainer').toggleClass('hidden');
+    }
+  });
+
+  $('.addToShelf').click(function(evt) {
+    evt.preventDefault();
+    $('.statusContainer').toggleClass('hidden');
+    $('.addBookProfile').toggleClass('hidden');
+  });
+
+
+//Cover Art Addition
+
+var $coverArtContainer = $('.appendedCoverArt');
+var $submitCoverImage  = $('#coverImage');
+var $bookCover         = $('.bookCover');
+
+$submitCoverImage.click(function() {
+  $bookCover.toggleClass('hidden');
+ var $pic = $('#coverUrl').val();
+ var $img = $('<img src='+$pic+'></img>');
+
+  $coverArtContainer.append($img);
+});
+
+//Cover Art Reset
+
+ var $coverReset = $('#reset');
+
+  $coverReset.click(function(evt) {
+    location.reload(true);
   });
